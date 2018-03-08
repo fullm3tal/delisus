defed6b22dec000633dfa0153616e9c29a06451a
fullm3tal <officialbheem3@gmail.com>
2018-03-08 17:55:32 +0000

    Voice recognition fix

--- app/src/main/java/com/example/dakaku/delisus/SearchActivity.java
+++ app/src/main/java/com/example/dakaku/delisus/SearchActivity.java
@@ -1,25 +1,34 @@
 package com.example.dakaku.delisus;
 
 import android.app.SearchManager;
+import android.app.SearchableInfo;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class SearchActivity extends AppCompatActivity {
 
     private static final String TAG = "SearchActivity";
 
+    @BindView(R.id.search_view)
+    SearchView searchView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        ButterKnife.bind(this);
 
         Toolbar toolbar=findViewById(R.id.app_searchBar);
         setSupportActionBar(toolbar);
-
         toolbar.setNavigationIcon(R.drawable.ic_arrow_back_black_24dp);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
@@ -29,11 +38,22 @@ public class SearchActivity extends AppCompatActivity {
             }
         });
 
+        //Setting up the SearchableInfo
+        SearchManager searchManager=(SearchManager)getSystemService(Context.SEARCH_SERVICE);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
     }
 
+
+    //onNewIntent() is used to display the voice response received from the voiceSearchButton into SearchView
+
     @Override
-    public boolean onSearchRequested() {
-        return super.onSearchRequested();
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        if(Intent.ACTION_SEARCH.equals(intent.getAction())){
+            String extraQuery=intent.getStringExtra(SearchManager.QUERY);
+            searchView.setQuery(String.valueOf(extraQuery),false);
+        }
     }
 
     @Override
